59a10a431e5261d706e2f77321a9a2721333f2d2
mahmoudmosalamdev <Mahmoud.Mosalam.dev@gmail.com>
2026-05-12 09:36:08 +0300

    Initial Commit

---
+++ WorldNews/WorldNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldNewsApp.swift
+//  WorldNews
+//
+//  Created by Mahmoud Mosalam on 12/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct WorldNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
